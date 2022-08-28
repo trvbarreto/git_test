c03818ce89463366c99020fb45fac0608fad9e98
Thales Barreto <trvbarreto@gmail.com>
2022-08-28 19:57:38 -0300

    Add shift and unshift examples

--- array-methods/script.js
+++ array-methods/script.js
@@ -19,3 +19,13 @@ console.log(humanBuildings); // (3) ['Town Hall', 'Barracks', 'Farm']
 let pushed = humanBuildings.push("Lumber Mill");
 console.log(pushed); // 4
 console.log(humanBuildings); // ['Town Hall', 'Barracks', 'Farm', 'Lumber Mill']
+
+// shift() remove o primeiro elemento e move todos os outros pra um indice a menos, e retorna o elemento que foi removido
+let shifted = humanBuildings.shift();
+console.log(shifted); // Town Hall
+console.log(humanBuildings); // (3) ['Barracks', 'Farm', 'Lumber Mill']
+
+// unshift() adiciona o elemento na primeira posição, e retorna o novo tamanho do array
+let unshifted = humanBuildings.unshift("Town Hall");
+console.log(unshifted); // 4
+console.log(humanBuildings); // (4) ['Town Hall', 'Barracks', 'Farm', 'Lumber Mill']
