b60c4d3f4d989ce274c34dadca785465289949de
Thales Barreto <trvbarreto@gmail.com>
2022-08-29 13:43:05 -0300

    Add filter() example, Refactor map example

--- loops/script.js
+++ loops/script.js
@@ -10,5 +10,14 @@ function toUpper(string) {
     return string.toUpperCase();
 }
 
-const upperUndeads = undeads.map(toUpper);
-console.log(upperUndeads); // (4) ['ACOLYTE', 'ABOMINATION', 'GHOUL', 'BANSHEE']
+const mapped = undeads.map(toUpper);
+console.log(mapped); // (4) ['ACOLYTE', 'ABOMINATION', 'GHOUL', 'BANSHEE']
+
+// filter() testa os items do array e os que passarem no teste são inseridos no novo array
+function startWithA(string) {
+    return string.startsWith('A');
+}
+
+const filtered = undeads.filter(startWithA);
+
+console.log(filtered); // (2) ['Acolyte', 'Abomination']
