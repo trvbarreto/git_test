12465804b4e17f3ee5bc17295a0748ddb0198e30
Thales Barreto <trvbarreto@gmail.com>
2022-11-21 14:52:36 -0300

    Criado middleware para checar se o usuário existe

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid'); // v4 é a versão que gera número aleatório, renomeamos para 'uuidv4'
 
@@ -6,6 +7,21 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return res.status(400).json({ error: 'Customer not found'});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
 
 app.post('/account', (req, res) => {
     const { cpf, name } = req.body;
@@ -24,6 +40,11 @@ app.post('/account', (req, res) => {
     });
 
     return res.status(201).send();
+});
+
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = request;
+    return res.json(customer.statement);
 });
 
 app.listen(3333);
