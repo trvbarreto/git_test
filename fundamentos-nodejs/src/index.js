3d4c70473b1c4be09f066c213fc54330b113a0e9
Thales Barreto <trvbarreto@gmail.com>
2022-11-22 10:02:19 -0300

    Remover conta

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -125,4 +125,12 @@ app.get('/account', verifyIfExistsAccountCPF, (req, res) => {
     return res.json(customer);
 });
 
+app.delete('/account', verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
+
+    customers.splice(customer, 1);
+
+    return res.status(200).json(customers);
+});
+
 app.listen(3333);
