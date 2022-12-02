5694daa0098459080d01cba0d10de4fcff4c91ba
Thales Barreto <trvbarreto@gmail.com>
2022-12-02 11:00:53 -0300

    Criando categoria

--- rocketseat/nodejs/ch2/rentalx/src/server.ts
+++ rocketseat/nodejs/ch2/rentalx/src/server.ts
@@ -1,16 +1,12 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.json("Hello World");
-});
-
-app.post("/courses", (req, res) => {
-    const { name } = req.body;
-    return res.json(name);
-});
+// Importando rota
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running"));
