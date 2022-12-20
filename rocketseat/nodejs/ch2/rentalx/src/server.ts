d729fd363eb9533411075cb8696d66736f5576ad
Thales Barreto <trvbarreto@gmail.com>
2022-12-20 17:20:12 -0300

    Criando repositório de especificação

--- rocketseat/nodejs/ch2/rentalx/src/server.ts
+++ rocketseat/nodejs/ch2/rentalx/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationRoutes } from "./routes/specification.routes";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 // Importando rota que sempre vai considerar o path inicial /categories
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationRoutes);
 
-app.listen(3333, () => console.log("🚀 Server is running!"));
+app.listen(3333, () => console.log("🚀🚀🚀 Server is running!"));
