85325d6284c9604866b98a1de0107853f8422259
Thales Barreto <trvbarreto@gmail.com>
2022-12-20 16:17:17 -0300

    Criando service de especificação e separando em módulos

---
+++ rocketseat/nodejs/ch2/rentalx/src/modules/cars/model/Specification.ts
@@ -0,0 +1,18 @@
+class Specification {
+    id?: string;
+    name: string;
+    description: string;
+    created_at: Date;
+
+    /**
+     * esse construtor verificar se já tem ID criado para a categoria
+     *  e cria se não houver
+     */
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
+
+export { Specification };
