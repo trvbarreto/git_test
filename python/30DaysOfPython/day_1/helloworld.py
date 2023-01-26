61902588b1d43454b94e660157196ff37d4dd0bb
Thales Barreto <trvbarreto@gmail.com>
2023-01-25 23:56:16 -0300

    Resolve exercise:Level2 from day_1

---
+++ python/30DaysOfPython/day_1/helloworld.py
@@ -0,0 +1,29 @@
+print(f'Addition 3+4 = {3+4}')
+print(f'Subtraction 3-4 = {3-4}')
+print(f'Multiplication 3*4 = {3*4}')
+print(f'Modulus 4%3 = {4%3}')
+print(f'Division 3/4 = {3/4}')
+print(f'Exponential 3**4 = {3**4}')
+print(f'Floor division operator 4//3 = {4//3}')
+
+print()
+
+my_name = 'Thales Barreto'
+my_family_name = 'Varanda Barreto'
+my_country = 'Brazil'
+str = 'I am enjoying 30 days of python'
+print(my_name)
+print(my_family_name)
+print(my_country)
+print(str)
+
+print()
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(my_name))
+print(type(my_family_name))
+print(type(my_country))
