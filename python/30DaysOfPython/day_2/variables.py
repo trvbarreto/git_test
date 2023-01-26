ddd59540b9e6b661469bb8efe3901775d594b5b0
Thales Barreto <tbarreto@star-su.com.br>
2023-01-26 10:46:00 -0300

    Resolve exercise:level1 from day_2

---
+++ python/30DaysOfPython/day_2/variables.py
@@ -0,0 +1,13 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Thales'
+last_name = 'Barreto'
+full_name = 'Thales Barreto'
+country = 'Brazil'
+city = 'Jundiaí'
+age = 25
+year = 2023
+is_married = False
+is_true = True
+is_light_on = True
+a, b, c = 1, 2, 3
