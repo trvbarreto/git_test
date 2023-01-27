ff7500290d507f5439d18adecd3a7ede20f69fcd
Thales <trvbarreto@gmail.com>
2023-01-27 10:17:19 -0300

    Resolve part of exercises from day_3

---
+++ python/30DaysOfPython/day_3/operators.py
@@ -0,0 +1,24 @@
+# my_age = 25
+# my_height = 1.76
+# complex = 1j
+
+# t_base = input('Enter the triangle base: ')
+# t_height = input('Enter the triangle height: ')
+# area = 0.5 * (float(t_base) * float(t_height))
+# print(f'The area is {area}')
+
+# side_a = int(input('Enter side a: '))
+# side_b = int(input('Enter side b: '))
+# side_c = int(input('Enter side c: '))
+# perimeter = side_a + side_b + side_c
+# print(f'The perimeter is {perimeter}')
+
+# import math
+# t_length = int(input('Enter the length: '))
+# t_width = int(input('Enter the width: '))
+# area = t_length * t_width
+# perimeter = 2 * (t_length + t_width)
+
+# circle_radius = int(input('Enter the radius: '))
+# area = math.pi * (circle_radius * circle_radius)
+# c_perimeter = 2 * (math.pi * circle_radius)
