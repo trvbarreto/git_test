0d338a7b6afc9dd4698c55e81d4823b69a53d9e2
Thales Barreto <trvbarreto@gmail.com>
2023-02-03 22:08:16 -0300

    Resolver exercise 1 to 11 from day_4

---
+++ python/30DaysOfPython/day_4/strings.py
@@ -0,0 +1,15 @@
+ex1 = 'Thirty ' + 'Days ' + 'Of ' + 'Python'
+print(ex1)
+ex2 = 'Coding ' + 'For ' + 'All '
+print(ex2)
+company = 'coding for ALL'
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company[:6])
+print(company.find('for'))
+print(company.replace('ALL', 'none'))
