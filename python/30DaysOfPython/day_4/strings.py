79d178f4df30dc5e8d7b7c272164d3a6a6cb9e77
Thales Barreto <trvbarreto@gmail.com>
2023-02-05 17:59:18 -0300

    Finish Day 4

--- python/30DaysOfPython/day_4/strings.py
+++ python/30DaysOfPython/day_4/strings.py
@@ -13,16 +13,16 @@
 # print(company[:6])
 # print(company.find('for'))
 # print(company.replace('coding', 'Python'))
-str = 'Coding For All'
-print('Python for Everyone'.replace('Everyone', 'All'))
-print(str.split(' '))
+# str = 'Coding For All'
+# print('Python for Everyone'.replace('Everyone', 'All'))
+# print(str.split(' '))
 
-str_big_techs = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
-print(str_big_techs.split(','))
+# str_big_techs = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+# print(str_big_techs.split(','))
 
-print(str[0])
-print(str[-1])
-print(str[10])
+# print(str[0])
+# print(str[-1])
+# print(str[10])
 
 # str = 'Python For Everyone'
 # str_arr = str.split(' ')
@@ -36,11 +36,20 @@ print(str[10])
 #     print(word[0], end='')
 # print()
 
-print(str.index('C'), str.index('F'), str.find('l'))
-print(str.rfind('l'))
-print('You cannot end a sentence with because because because is a conjunction'.find('because'))
-print('You cannot end a sentence with because because because is a conjunction'.rfind('because'))
-print(
-    'You cannot end a sentence with because because because is a conjunction'[31:54])
-print(str.startswith('Coding'))
-print(str.endswith('coding'))
+# print(str.index('C'), str.index('F'), str.find('l'))
+# print(str.rfind('l'))
+# print('You cannot end a sentence with because because because is a conjunction'.find('because'))
+# print('You cannot end a sentence with because because because is a conjunction'.rfind('because'))
+# print(
+#     'You cannot end a sentence with because because because is a conjunction'[31:54])
+# print(str.startswith('Coding'))
+# print(str.endswith('coding'))
+# print('   Coding For All      '.lstrip())
+# print('30DaysOfPython'.isidentifier())
+# print('thirty_days_of_python'.isidentifier())
+# print(' # '.join(['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']))s
+# print('Name\tAge\tCountry\tCity\tAsabeneh\t250\tFinland\tHelsinki')
+# str = 'radius = 10 \narea = 3.14 * radius ** 2 \nThe area of a circle with radius 10 is 314 meters square.'
+# print(str)
+str = '8 + 6 = 14\n8 - 6 = 2\n8 * 6 = 48\n8 / 6 = 1.33\n8 % 6 = 2\n8 // 6 = 1\n8 ** 6 = 262144'
+print(str)
