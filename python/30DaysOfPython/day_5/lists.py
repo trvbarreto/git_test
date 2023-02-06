3d8a86042ba852e8536baeb6e0a02bf5a1ba0263
Thales <trvbarreto@gmail.com>
2023-02-06 16:48:19 -0300

    Resolve exercise1: 1-10 from day_5

---
+++ python/30DaysOfPython/day_5/lists.py
@@ -0,0 +1,24 @@
+# 1
+list1 = []
+# 2
+list1 = ['Zeus', 'Odin', 'Hades', 'Thor', 'Seth']
+# 3
+print(len(list1))
+# 4
+print(list1[0])
+print(list1[2])
+print(list1[-1])
+# 5
+mixed_data_types = ['Thales', 25, '1,76m', 'Single', 'Jundiaí-SP']
+# 6
+it_companies = ['Facebook', 'Google', 'Microsoft',
+                'Apple', 'IBM', 'Oracle', 'Amazon']
+# 7
+print(it_companies)
+# 8
+print(len(it_companies))
+# 9
+print(it_companies[0], it_companies[3], it_companies[-1])
+# 10
+it_companies[2] = 'Alibaba'
+print(it_companies)
