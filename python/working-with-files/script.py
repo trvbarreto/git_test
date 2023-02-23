169c2e93a46f33985e63acad9e1e00f5a92f3939
Thales <trvbarreto@gmail.com>
2023-02-23 11:41:10 -0300

    187. Read, Write, Append

--- python/working-with-files/script.py
+++ python/working-with-files/script.py
@@ -1,4 +1,4 @@
-my_file = open('test.txt')
+# my_file = open('test.txt')
 
 # print(my_file.read())
 # print(my_file.read())
@@ -10,4 +10,16 @@ my_file = open('test.txt')
 
 # print(my_file.readlines())
 
-my_file.close()
+# my_file.close() # signals that the use of ile is over
+
+# This way (with) there is no need for closing the file
+with open('test.txt', mode='r') as my_file:
+    print(my_file.readlines())
+
+with open('test.txt', mode='a') as my_file:
+    text = my_file.write(':)')
+    print(text)
+
+with open('sad.txt', mode='w') as my_file:
+    text = my_file.write(':(')
+    print(text)
