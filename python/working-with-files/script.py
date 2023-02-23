bf613e95dc7ed1cd5266b0552dd47d4e5bdc06ff
Thales <trvbarreto@gmail.com>
2023-02-23 11:47:10 -0300

    188. File IO Errors

--- python/working-with-files/script.py
+++ python/working-with-files/script.py
@@ -12,14 +12,25 @@
 
 # my_file.close() # signals that the use of ile is over
 
-# This way (with) there is no need for closing the file
-with open('test.txt', mode='r') as my_file:
-    print(my_file.readlines())
+# # This way (with) there is no need for closing the file
+# with open('test.txt', mode='r') as my_file:
+#     print(my_file.readlines())
 
-with open('test.txt', mode='a') as my_file:
-    text = my_file.write(':)')
-    print(text)
+# with open('test.txt', mode='a') as my_file:
+#     text = my_file.write(':)')
+#     print(text)
 
-with open('sad.txt', mode='w') as my_file:
-    text = my_file.write(':(')
-    print(text)
+# with open('sad.txt', mode='w') as my_file:
+#     text = my_file.write(':(')
+#     print(text)
+
+# File IO Erros
+try:
+    with open('teste.txt', mode='x') as my_file:
+        print(my_file.read())
+except FileNotFoundError as err:
+    print('file does not exist')
+    raise err
+except IOError as err:
+    print('IO Error')
+    raise err
