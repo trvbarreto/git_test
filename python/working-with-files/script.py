562d1225d20302c2a682e080ff598bd76deafd84
Thales <trvbarreto@gmail.com>
2023-02-23 11:31:07 -0300

    182. Working with file in python

---
+++ python/working-with-files/script.py
@@ -0,0 +1,13 @@
+my_file = open('test.txt')
+
+# print(my_file.read())
+# print(my_file.read())
+# print(my_file.read())
+
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+
+# print(my_file.readlines())
+
+my_file.close()
