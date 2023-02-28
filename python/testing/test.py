5fbbe3baba7e5470e141e03bef4586c4bc470f1a
Thales <trvbarreto@gmail.com>
2023-02-28 16:59:36 -0300

    189. Writing Unit Tests

--- python/testing/test.py
+++ python/testing/test.py
@@ -11,7 +11,18 @@ class TestMain(unittest.TestCase):
     def test_do_stuff2(self):
         test_param = 'asda'
         result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_stuff3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
+
+    def test_do_stuff4(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
