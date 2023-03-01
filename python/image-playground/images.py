850352502c3d4340deac50219fff204f6ebba10a
Thales <trvbarreto@gmail.com>
2023-03-01 15:12:27 -0300

    215. Solution: JPG to PNG pokedex converter

--- python/image-playground/images.py
+++ python/image-playground/images.py
@@ -1,28 +1,28 @@
-from PIL import Image, ImageFilter
+# from PIL import Image, ImageFilter
 
-img = Image.open('./pokedex/pikachu.jpg')
-# img.format
-# img.size
-# img.mode
+# img = Image.open('./pokedex/pikachu.jpg')
+# # img.format
+# # img.size
+# # img.mode
 
-filtered_img = img.filter(ImageFilter.SHARPEN)
-# ImageFilter.BLUR
-# ImageFilter.SMOOTH
-# ImageFilter.SHARPEN
-filtered_img.save('filtered.png', 'png')
+# filtered_img = img.filter(ImageFilter.SHARPEN)
+# # ImageFilter.BLUR
+# # ImageFilter.SMOOTH
+# # ImageFilter.SHARPEN
+# filtered_img.save('filtered.png', 'png')
 
-converted_img = img.convert('L')
-converted_img.save('converted.png', 'png')
+# converted_img = img.convert('L')
+# converted_img.save('converted.png', 'png')
 
-# filtered_img.show()
-# converted_img.show()
+# # filtered_img.show()
+# # converted_img.show()
 
-crooked = filtered_img.rotate(180)
-crooked.save('upside.png', 'png')
+# crooked = filtered_img.rotate(180)
+# crooked.save('upside.png', 'png')
 
-resize = filtered_img.resize((300, 300))
-resize.save('resized.png', 'png')
+# resize = filtered_img.resize((300, 300))
+# resize.save('resized.png', 'png')
 
-box = (100, 100, 400, 400)
-region = filtered_img.crop(box)
-region.save('croped.png', 'png')
+# box = (100, 100, 400, 400)
+# rregion = filtered_img.crop(box)
+# rregion.save('croped.png', 'png')
--- python/image-playground/images2.py
+++ python/image-playground/images2.py
@@ -1,16 +1,16 @@
-from PIL import Image
+# from PIL import Image
 
-img = Image.open('./deer.jpg')
-print(img.size)
-new_img = img.resize((400, 400))
-new_img.save('deer-resized.png', 'png')
-new_img.save('deer-resized2.png', 'png')
+# img = Image.open('./deer.jpg')
+# print(img.size)
+# new_img = img.resize((400, 400))
+# new_img.save('deer-resized.png', 'png')
+# new_img.save('deer-resized2.png', 'png')
 
-deer = Image.open('./deer.jpg')
-deer.thumbnail((400, 400))
-deer.save('thumbnail.png')
+# deer = Image.open('./deer.jpg')
+# deer.thumbnail((400, 400))
+# deer.save('thumbnail.png')
 
-print(new_img.size)
-print(deer.size)
+# print(new_img.size)
+# print(deer.size)
 
-# Thumbnail tries to resize the best way possible within the passed tuple (400,400)
+# # Thumbnail tries to resize the best way possible within the passed tuple (400,400)
