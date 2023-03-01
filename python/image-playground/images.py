bf8d5784fd4ae22ec9a864d4ce88f1c1f171ee0e
Thales <trvbarreto@gmail.com>
2023-03-01 14:41:40 -0300

    211. Images With Python 2

--- python/image-playground/images.py
+++ python/image-playground/images.py
@@ -11,5 +11,18 @@ filtered_img = img.filter(ImageFilter.SHARPEN)
 # ImageFilter.SHARPEN
 filtered_img.save('filtered.png', 'png')
 
-coverted_img = img.convert('L')
-coverted_img.save('converted.png', 'png')
+converted_img = img.convert('L')
+converted_img.save('converted.png', 'png')
+
+# filtered_img.show()
+# converted_img.show()
+
+crooked = filtered_img.rotate(180)
+crooked.save('upside.png', 'png')
+
+resize = filtered_img.resize((300, 300))
+resize.save('resized.png', 'png')
+
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
+region.save('croped.png', 'png')
