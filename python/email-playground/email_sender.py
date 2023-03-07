bad2cccafab52ce32686283c46b2665a3e815fd5
Thales <trvbarreto@gmail.com>
2023-03-07 17:22:09 -0300

    224. Sending Emails With Python 2

--- python/email-playground/email_sender.py
+++ python/email-playground/email_sender.py
@@ -1,2 +1,19 @@
 import smtplib
 from email.message import EmailMessage
+
+email_address = 'trvb.test@gmail.com'
+email_password = 'lpgvojojhnxkfenw'
+
+email = EmailMessage()
+email['from'] = 'Gran Torino'
+email['to'] = 'trvb.test@gmail.com'
+email['subject'] = 'You won 1.000.000 dollars!'
+
+email.set_content('I am a Python Master!')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(email_address, email_password)
+    smtp.send_message(email)
+    print('all good boss!!')
