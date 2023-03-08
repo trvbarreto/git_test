21e914eb7cddc2c79be36b1c6fd26706bff163b2
Thales <trvbarreto@gmail.com>
2023-03-08 08:57:00 -0300

    231. Password Checker

--- python/passwordchecker/checkmypass.py
+++ python/passwordchecker/checkmypass.py
@@ -1,4 +1,19 @@
 import requests
-url = 'https://api.pwnedpasswords.com/range/' + 'BAB12'
-res = requests.get(url)
-print(res)
+import hashlib
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f'Error fetching: {res.status_code}, check the api and try again.')
+    return res
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    return sha1password
+
+
+pwned_api_check('123')
