db8fcaf8d7d94b9a9342ffc73bcd93890fa54e29
Thales <trvbarreto@gmail.com>
2023-03-08 09:16:33 -0300

    233. Password Checker 3

--- python/passwordchecker/checkmypass.py
+++ python/passwordchecker/checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -14,7 +15,9 @@ def request_api_data(query_char):
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
-        print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
 
 
 def pwned_api_check(password):
@@ -25,4 +28,15 @@ def pwned_api_check(password):
     return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times...you should probably change your password')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+    return '✔️ Done'
+
+
+main(sys.argv[1:])
