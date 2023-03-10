57c26444d4e83693c6b66b0aeba6f1c4551f2280
Thales <trvbarreto@gmail.com>
2023-03-10 10:40:17 -0300

    254. Hacker News Project

--- python/scraping-playground/scrape.py
+++ python/scraping-playground/scrape.py
@@ -7,3 +7,16 @@ response = requests.get('https://news.ycombinator.com/newest')
 soupObject = BeautifulSoup(response.text, 'html.parser')
 links = soupObject.select('.titleline')
 votes = soupObject.select('.score')
+
+
+def create_custom_hackernews(links, votes):
+    hackernews = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        href = links[index].get('href', None)
+        points = int(votes[index].getText().replace(' points', ''))
+        hackernews.append({'title': title, 'link': href})
+    return hackernews
+
+
+print(create_custom_hackernews(links, votes))
