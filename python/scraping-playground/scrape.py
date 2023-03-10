de6fb0def50ac9f1f3d29654e034fcd6bf37e93b
Thales <trvbarreto@gmail.com>
2023-03-10 10:32:59 -0300

    253. BeautifulSoup Selectors

--- python/scraping-playground/scrape.py
+++ python/scraping-playground/scrape.py
@@ -5,4 +5,5 @@ from bs4 import BeautifulSoup
 response = requests.get('https://news.ycombinator.com/newest')
 
 soupObject = BeautifulSoup(response.text, 'html.parser')
-print(soupObject.find('a'))
+links = soupObject.select('.titleline')
+votes = soupObject.select('.score')
